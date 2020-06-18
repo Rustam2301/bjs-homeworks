112180677687f03bfe3e2cc6ce76657257e7d7f6
Rustam2301 <62401714+Rustam2301@users.noreply.github.com>
2020-06-19 01:34:58 +0600

    Add files via upload

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,54 @@
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    let x = [];
+    let d = b**2-4*a*c;
+
+    if (d < 0) {
+    	x = undefined;
+    } else if (d === 0) {
+    	x[0] = (-b + Math.sqrt(d)) / (2 * a);
+    } else if (d > 0) {
+    	x[0] = (-b + Math.sqrt(d)) / (2 * a);
+    	x[1] = (-b - Math.sqrt(d)) / (2 * a);
+    }
+    
+    return x;
 }
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark = 0;
+
+    if (!marks) {
+    	return 0;
+    } else if (marks.length > 5) {
+    	console.log(`Количество введенных оценок: ${marks.length}, среднее значение рассчитано для первых 5`);
+    	marks = marks.slice(0, 5);
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+    	averageMark += marks[i];
+    }
+
+    averageMark /= marks.length;
+
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    // return result;
+    let today = new Date();
+    today = today.getFullYear();
+    let year = dateOfBirthday.getFullYear();
+    let yearsOld = (today - year);
+    let result;    
+
+    if (yearsOld >= 18) {
+    	result = `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+    	result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+
+    return result;
 }
