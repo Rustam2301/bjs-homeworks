aafa97b2f25ce2aa97d072577ae14bc3c68d0022
Rustam2301 <62401714+Rustam2301@users.noreply.github.com>
2020-07-23 01:31:02 +0600

    Add files via upload

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,39 @@
 //String.prototype.isPalindrome - для задачи №1
 
+function getAnimalSound(animal) {
+
+	if (animal === undefined) {
+		return null;
+	}
+	
+	let sound = animal.sound;
+	return sound;
+	
+}
+
+
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+    let average = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+    	average += marks[i];
+    }
+
+    average /= marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
+    let now = new Date();
+    let birthday = Date.parse(birthday);
+    let diff = now - birthday;    
+    const msInYear = Number(3.154e+10 + 2.16e+7);
+    let age = Math.floor(diff/msInYear);
+
+    if (age >= 18) {return 'Совершеннолетний'};
+    return 'Нет';
     // return verdict
 }
